af6368f35a32434746fdd020c863721f8a9362d7
Daniel Duarte <danieldd.ar@gmail.com>
2020-02-02 22:27:46 -0300

    Created components for burger and ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
   render() {
     return (
       <Fragment>
-        <div>Burguer</div>
+        <Burger/>
         <div>Controls</div>
       </Fragment>
     );
