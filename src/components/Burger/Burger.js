7595ef64fc5287cec89b0c0f9a365ee7124cc878
Daniel Duarte <danieldd.ar@gmail.com>
2020-02-02 23:52:07 -0300

    Added ingredients as state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () => {
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map((ingrKey) => {
+      return [...Array(props.ingredients[ingrKey])].map((_, i) => (
+        <BurgerIngredient key={ingrKey + i} type={ingrKey} />
+      ));
+    });
+
   return (
-    <BurgerIngredient type="meat"/>
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
   );
 };
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from './Layout.module.css';
 
 const layout = (props) => (
   <Fragment>
-    <div>Toolbar, Sidebar Backdrop</div>
+    <div>Toolbar, Sidebar, Backdrop</div>
     <main className={classes.Content}>
       {props.children}
     </main>
