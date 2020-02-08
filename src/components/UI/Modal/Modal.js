b92cbe514c4b53c1017ffe4ad06347eb5ddcb306
Daniel Duarte <danieldd.ar@gmail.com>
2020-02-07 21:05:25 -0300

    Improvements: Performance on Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Modal.module.css'
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Fragment>
-    <Backdrop show={props.show} onClick={props.modalClosed}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}>
-      {props.children}
-    </div>
-  </Fragment>
-);
+class Modal extends Component {
 
-modal.propTypes = {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} onClick={this.props.modalClosed}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+Modal.propTypes = {
   show: PropTypes.bool,
   modalClosed: PropTypes.func,
 };
 
-export default modal;
+export default Modal;
