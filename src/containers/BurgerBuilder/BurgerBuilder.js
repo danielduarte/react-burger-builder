2d2ddb9bd172ba15d7c0f3860a443b274ccedd7c
Daniel Duarte <danieldd.ar@gmail.com>
2020-02-09 13:45:03 -0300

    Added simple Firebase backend to store orders

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from 'react';
 
+import axios from '../../axios-orders';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -24,7 +26,8 @@ class BurgerBuilder extends Component {
     totalPrice: 4,
     purchasable: false,
     purchasing: false,
-  };
+    loading: false,
+  }
 
   updatePurchaseState = (ingredients) => {
     const ingredientsSum = Object.keys(ingredients)
@@ -65,7 +68,30 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('Thanks for your purchase!');
+    this.setState({ loading: true });
+
+    const data = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Daniel Duarte',
+        address: {
+          street: 'Fake St. 1234',
+          zipCode: 7001,
+          country: 'Argentina',
+        },
+        email: 'danieldd.ar@gmail.com',
+      },
+      deliveryMethod: 'cheaper',
+    };
+
+    axios.post('/orders.json', data)
+      .then(response => {
+        this.setState({ loading: false, purchasing: false });
+      })
+      .catch(error => {
+        this.setState({ loading: false, purchasing: false });
+      });
   };
 
   render() {
@@ -74,14 +100,22 @@ class BurgerBuilder extends Component {
       return acc;
     }, {});
 
+    let orderSummary = (
+      <OrderSummary
+        ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+        onContinue={this.purchaseContinueHandler}
+        onCancel={this.purchaseCancelHandle} />
+    );
+
+    if (this.state.loading) {
+      orderSummary = (<Spinner />);
+    }
+
     return (
       <Fragment>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandle}>
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            onContinue={this.purchaseContinueHandler}
-            onCancel={this.purchaseCancelHandle} />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
